df14aeabac731f89f0bb5c5c12cee5fc8c639956
Kennedy <kennedymessias.vb@gmail.com>
2020-11-30 00:02:26 -0300

    observer pattern

---
+++ theories/observer/weatherObserver/CurrentConditionsDisplay.java
@@ -0,0 +1,14 @@
+public class CurrentConditionsDisplay extends WeatherDataObserver {
+	
+	public CurrentConditionsDisplay(WeatherData data) {
+		super(data);
+	}
+
+	public void display() {
+		System.out.println(">>> Current conditions: ");
+		System.out.println("Temperature: " + data.getTemperature());
+		System.out.println("Humidity: " + data.getHumidity());
+		System.out.println("Pressure: " + data.getPressure());
+		
+	}
+}
---
+++ theories/observer/weatherObserver/WeatherDataObserver.java
@@ -0,0 +1,25 @@
+
+public abstract class WeatherDataObserver implements Observer {
+  WeatherData data;
+  
+  public WeatherDataObserver(WeatherData data) {
+    data.addObserver(this);
+		this.data = data;
+  }
+
+  @Override
+	public void update(Object data) {
+    this.data = (WeatherData)data;
+    this.display();
+  }
+  
+  protected abstract void display();
+
+  public WeatherData getData() {
+		return data;
+	}
+
+	public void setData(WeatherData data) {
+		this.data = data;
+	}
+}
